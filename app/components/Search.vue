a83898b5e1139019ef748aab524b269d587e29e6
James Rock <hello@jamesrock.me>
2025-09-18 15:04:46 +0100

    updates

--- app/components/Search.vue
+++ app/components/Search.vue
@@ -1,21 +1,40 @@
 <script setup lang="js">
-  import { media_type_profile_path, largest_size_map, media_type_name } from '~/lib/api';
-  // const props = defineProps(['films', 'link', 'credits', 'name']);
-  // const { films, link, credits, name = 'name' } = props;
-  // console.log(films);
-  const items = [];
+  import { api, addProp, sortByProp, filterByMatch, media_type_name, media_type_profile_path, largest_size_map } from '~/lib/api';
+  import { ref, onMounted } from 'vue';
+  
+  const query = defineModel();
+  let items = ref([]);
+  const cache = {};
+  const runSearch = () => {
+    if(query.value.length === 0) {
+      clearSearch();
+      return;
+    };
+    if(cache[query.value]) {
+      items = cache[query.value];
+      return;
+    };
+    api.search(query.value).then(data => {
+      cache[query.value] = sortByProp(filterByMatch([...addProp(data[0].results, 'media_type', 'movie'), ...addProp(data[1].results, 'media_type', 'person')], query.value), 'popularity');
+      items.value = cache[query.value];
+    }).catch(error => console.log('Error:', error));
+  };
+  const clearSearch = () => {
+    query.value = '';
+    items.value = [];
+  };
 </script>
 
 <template>
   <div class="search">
     <div class="search-body">
-      <input type="search" placeholder="Search film, actor, director" />
+      <input type="search" placeholder="Search film, actor, director" v-model="query" @input="runSearch" />
       <div class="search-results">
-        <NuxtLink :to="`/${item.media_type}/${item.id}`" class="search-results-item" v-for="item in items">
+        <NuxtLink :to="`/${item.media_type}/${item.id}`" class="search-results-item" v-for="item in items" :key="item.id" @click="clearSearch">
           <div class="poster-wrap">
             <Poster :path="item[media_type_profile_path[item.media_type]]" :size="largest_size_map[item.media_type]" />
           </div>
-          <div>{item[media_type_name[item.media_type]]}</div>
+          <div>{{item[media_type_name[item.media_type]]}}</div>
         </NuxtLink>
       </div>
     </div>
