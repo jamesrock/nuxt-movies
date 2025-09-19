26c5f68dd4266d0efd04156d1891aec40bb1fe10
James Rock <hello@jamesrock.me>
2025-09-19 21:51:37 +0100

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/image']
+  modules: ['@nuxt/image', '@nuxt/fonts'],
+  fonts: {
+    defaults: {
+      weights: ['400', '500', '600', '700'],
+      styles: ['italic', 'normal'],
+      subsets: ['latin']
+    }
+  }
 })
